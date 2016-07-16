d1ae39a18ee4e8b886b2db6a53751cdbf9f35b2d
Jonathan <jonathanlbs@gmail.com>
2016-07-16 01:21:56 -0300

    Feito até agora 16/07 01:19
    Mapa aparece mas ainda não é possível jogar a localização do usuário
    nele e criar a rota.

---
+++ teste_geolocalizacao/www/js/script.js
@@ -0,0 +1,54 @@
+// Menu hamburguer
+
+function slidetoggle() {
+      var slider = document.getElementById("content-menu");
+      // slider.style.height = (window.innerHeight -(window.innerHeight * 0,3)) + "px";
+      // slider.style.top = (window.innerHeight -(window.innerHeight * 0,3)) + "px";
+      document.getElementById("shadow").className += " is-active";
+      if(slider.style.left == "0px") {
+        slider.style.left = "-500px";
+        
+      }
+      else {
+        slider.style.left = "0px";
+      }
+    }
+function slideclose() {
+      var slider = document.getElementById("content-menu");
+      document.getElementById("shadow").className = "shadow";
+      slider.style.left = "-300px";
+    }
+
+document.addEventListener("deviceready", onDeviceReady , false);
+var latitude = null;
+var longitude = null;
+var meulocal = null;
+function onDeviceReady(){
+
+      $.mobile.loading("show");
+      navigator.geolocation.getCurrentPosition(onSuccess, onError, getOptions());
+    }
+        
+    var onSuccess = function(position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        window.alert("Latitude: " + latitude + " Longitude: " + longitude);
+        meulocal = latitude + "," + longitude;
+        window.alert("Meu local: " +meulocal);
+        $.mobile.loading("hide");
+    };
+
+    function onError(error) {
+        alert("Code: "    + error.code    + "\n" +
+            "Message: " + error.message + "\n");
+        $.mobile.loading("hide");
+    }
+
+    function getOptions() {
+        return {
+            maximumAge: 5000,
+            timeout: 10000,
+            enableHighAccuracy: true
+        }
+    }
+
