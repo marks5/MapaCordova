324b80a6263b21447cae6afe7ae60f28a6a9f77e
Jonathan <jonathanlbs@gmail.com>
2016-07-16 02:12:19 -0300

    SERÁ?
    PUTA QUE PARIU PARECE QUE FUNCIONOU

--- teste_geolocalizacao/platforms/android/assets/www/js/script.js
+++ teste_geolocalizacao/platforms/android/assets/www/js/script.js
@@ -2,8 +2,6 @@
 
 function slidetoggle() {
       var slider = document.getElementById("content-menu");
-      // slider.style.height = (window.innerHeight -(window.innerHeight * 0,3)) + "px";
-      // slider.style.top = (window.innerHeight -(window.innerHeight * 0,3)) + "px";
       document.getElementById("shadow").className += " is-active";
       if(slider.style.left == "0px") {
         slider.style.left = "-500px";
